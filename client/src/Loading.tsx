d18bc9c7764133763e80810b3d02de89e95152e3
hoangvu01 <hoangvu20092005@gmail.com>
2026-01-05 22:34:28 +0700

    reset(client): lấy lại toàn bộ client từ HoangKiew để làm lại từ đầu

--- client/src/Loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div style={{ padding: 24, textAlign: 'center' }}>
-      <div>Loading...</div>
-    </div>
-  )
-}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './router.tsx'
-import { AuthProvider } from './hooks/useAuth'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <AuthProvider>
-    <RouterProvider router={router} />
-  </AuthProvider>
+  <App />
 )
